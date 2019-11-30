3b0de2352683256ed58e9ab28024f6f72471cf52
abdo3017 <abdogazy14@gmail.com>
2019-11-30 22:05:35 +0200

    final

---
+++ app/src/main/java/com/example/medicalapp/API/WebServices.java
@@ -0,0 +1,54 @@
+package com.example.medicalapp.API;
+
+
+import com.example.medicalapp.API.DiagnosesModel.DiagnosesResponse;
+import com.example.medicalapp.API.DoctorsModel.DoctorResponse;
+import com.example.medicalapp.API.DrugModel.DrugInfoModel.DrugInfoResponse;
+import com.example.medicalapp.API.DrugModel.DrugResponse;
+import com.example.medicalapp.API.NewsModel.NewsResponse;
+import com.example.medicalapp.API.SymptomModel.SymptomResponse;
+
+import org.json.JSONArray;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface WebServices {
+
+// Rabid Api methods
+
+    @GET("symptoms")
+    Call<List<SymptomResponse>> getSymptoms(@Query("language")String language);
+
+
+
+
+    @GET("diagnosis")
+    Call<List<DiagnosesResponse>> getDiagnoses(@Query("symptoms") JSONArray symptomArray,@Query("gender")String gender,@Query("year_of_birth")int  year,@Query("language") String language);
+
+
+
+
+//DoctorApi
+@GET("doctors")
+Call<DoctorResponse>getDoctors(@Query("user_key")String apiKey,@Query("name")String name,@Query("limit")String limit);
+//DrugsApi
+    @GET("drugs")
+    Call<DrugResponse> getDrugs(@Query("page")String page,@Query("count")String count);
+
+    @GET("drugs/{chembl_id}")
+    Call<DrugInfoResponse>getDrugInfo(@Path("chembl_id")String chembl_idٍ);
+
+
+    //InferamidecaApi
+
+    //NewsApi
+    @GET("everything")
+    Call<NewsResponse>getNews(@Query("q")String Q,@Query("apiKey")String ApiKey);
+
+}
---
+++ app/src/main/java/com/example/medicalapp/Constants.java
@@ -0,0 +1,27 @@
+package com.example.medicalapp;
+
+import android.content.SharedPreferences;
+
+public class Constants {
+
+    //Rapid Api
+   public static  String  RAPID_API_KEY;
+   public static  String  RAPID_API_PROJECT_NAME;
+   public static  String  RAPID_API_HOST;
+
+    public static  User  CURRENT_USER;
+//inferamedica Api
+    public static  String INFREMIDECA_API_KEY;
+    public static  String INFERAMIDICA_APLICATION_ID;
+
+//News Api
+    public static  String NEWS_API;
+
+    //DoctorApiKey
+
+    public static  String DOCTORAPI;
+
+    //Languages
+    public static  String Language;
+
+}
